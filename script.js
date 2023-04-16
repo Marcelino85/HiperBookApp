76eff2da529b3b684f83fbdc94e1ddf084721f05
Marcelino Albuquerque <marcelino85albuquerque@gmail.com>
2023-04-16 17:19:16 -0300

    Inition Hiper book

---
+++ script.js
@@ -0,0 +1,4 @@
+const numeros = ['escola', 'casa', 'inverno', 'macaco', 'estudantes'];
+const numero = Math. floor(Math. random() * numeros. length);
+
+document.  write(numeros[numero]); // resultado aleatório.
