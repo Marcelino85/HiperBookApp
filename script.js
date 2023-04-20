f1369c935cb1e9cd0e57ac43b004a018f60908f5
Marcelino Albuquerque <marcelino85albuquerque@gmail.com>
2023-04-20 08:44:39 -0300

    Inicio do HiperBook

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const numeros = ['escola', 'casa', 'inverno', 'macaco', 'estudantes'];
-const numero = Math. floor(Math. random() * numeros. length);
+let numeroSorteado = document.querySelector("div.container h1")
+const botaoSortear = document.querySelector("div.container button")
 
-document.  write(numeros[numero]); // resultado aleatório.
+const palavras= new Array()
+palavras[0] = "'Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar.' (Js 1.9)";
+
+palavras[1] = "'Eu disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo'. (Jo 16:33)";
+
+palavras[2] = "'Se o meu povo, que se chama pelo meu nome, se humilhar e orar, buscar a minha face e se afastar dos seus maus caminhos, dos céus o ouvirei, perdoarei o seu pecado e curarei a sua terra'. (2 Cronicas 7.14";
+
+palavras[3] = "'Busquem, pois, em primeiro lugar o Reino de Deus e a sua justiça, e todas essas coisas serão acrescentadas a vocês'.(Mt 6.33)";
+
+palavras[4] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. (Rm 12.2)";
+
+palavras[5] = "'E, agora, que o Deus da paz os torne santos em todos os aspectos, e que o espírito, a alma e o corpo de vocês sejam mantidos irrepreensíveis até a volta de nosso Senhor Jesus Cristo'. (1 Ts 5:23)";
+
+
+
+botaoSortear.addEventListener("click", ()=>{
+
+numeroSorteado.innerHTML = "<h3>Palavra do dia:</h3><br> " + palavras[
+Math.floor(Math. random() * palavras. length)
+
+]
+
+
+})
