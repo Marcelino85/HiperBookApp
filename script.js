bc605b973bb9aff1b425e52632378292cf1fd230
Marcelino Albuquerque <marcelino85albuquerque@gmail.com>
2023-04-23 02:11:23 -0300

    Responsividade

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ palavras[4] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela 
 
 palavras[5] = "'E, agora, que o Deus da paz os torne santos em todos os aspectos, e que o espírito, a alma e o corpo de vocês sejam mantidos irrepreensíveis até a volta de nosso Senhor Jesus Cristo'. (1 Ts 5:23)";
 
+palavras[6] = "'Tudo posso naquele que me fortalece'. (Fl 4.13)";
+
+palavras[7] = "'Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus'. (1Ts 5.18)";
+palavras[8] = "'Então disse Jesus: 'Deixem vir a mim as crianças e não as impeçam; pois o Reino dos céus pertence aos que são semelhantes a elas'. (Mt 19.14)";
+
+palavras[9] = "'Consagre ao Senhor tudo o que você faz, e os seus planos serão bem-sucedidos'. (Pv 16.3)";
+
+palavras[10] = "'Quem tem muitos amigos pode chegar à ruína, mas existe amigo mais apegado que um irmão'. (Pv 18.24)";
+
+palavras[10] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. (Rm 12.1)";
+
 
 
 botaoSortear.addEventListener("click", ()=>{
