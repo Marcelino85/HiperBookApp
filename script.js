33508871b1e21a4aaab34e6e0be5e4d6a0bca276
Marcelino Albuquerque <marcelino85albuquerque@gmail.com>
2023-05-25 03:29:04 -0300

    Atualização do App

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-let numeroSorteado = document.querySelector("div.container h1")
-const botaoSortear = document.querySelector("div.container button")
+const palavraSortida=document.querySelectorAll('div.container h1')[0]
+const btn=document.getElementById('contat2')
 
 const palavras= new Array()
-palavras[0] = "'Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar.' (Js 1.9)";
+palavras[0] = "'Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar.' <br>(Js 1.9)</br>";
 
-palavras[1] = "'Eu disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo'. (Jo 16:33)";
+palavras[1] = "'Eu disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo'. <br>(Jo 16:33)</br>";
 
-palavras[2] = "'Se o meu povo, que se chama pelo meu nome, se humilhar e orar, buscar a minha face e se afastar dos seus maus caminhos, dos céus o ouvirei, perdoarei o seu pecado e curarei a sua terra'. (2 Cronicas 7.14)";
+palavras[2] = "'Se o meu povo, que se chama pelo meu nome, se humilhar e orar, buscar a minha face e se afastar dos seus maus caminhos, dos céus o ouvirei, perdoarei o seu pecado e curarei a sua terra'. <br>(2 Cronicas 7.14)</br>";
 
-palavras[3] = "'Busquem, pois, em primeiro lugar o Reino de Deus e a sua justiça, e todas essas coisas serão acrescentadas a vocês'.(Mt 6.33)";
+palavras[3] = "'Busquem, pois, em primeiro lugar o Reino de Deus e a sua justiça, e todas essas coisas serão acrescentadas a vocês'.<br>(Mt 6.33)</br>";
 
-palavras[4] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. (Rm 12.2)";
+palavras[4] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. <br>(Rm 12.2)</br>";
 
-palavras[5] = "'E, agora, que o Deus da paz os torne santos em todos os aspectos, e que o espírito, a alma e o corpo de vocês sejam mantidos irrepreensíveis até a volta de nosso Senhor Jesus Cristo'. (1 Ts 5:23)";
+palavras[5] = "'E, agora, que o Deus da paz os torne santos em todos os aspectos, e que o espírito, a alma e o corpo de vocês sejam mantidos irrepreensíveis até a volta de nosso Senhor Jesus Cristo'. <br>(1 Ts 5:23)</br>";
 
-palavras[6] = "'Tudo posso naquele que me fortalece'. (Fl 4.13)";
+palavras[6] = "'Tudo posso naquele que me fortalece'. <br>(Fl 4.13)</br>";
 
-palavras[7] = "'Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus'. (1Ts 5.18)";
-palavras[8] = "'Então disse Jesus: 'Deixem vir a mim as crianças e não as impeçam; pois o Reino dos céus pertence aos que são semelhantes a elas'. (Mt 19.14)";
+palavras[7] = "'Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus'. <br>(1Ts 5.18)</br>";
+palavras[8] = "'Então disse Jesus: 'Deixem vir a mim as crianças e não as impeçam; pois o Reino dos céus pertence aos que são semelhantes a elas'. <br>(Mt 19.14)</br>";
 
-palavras[9] = "'Consagre ao Senhor tudo o que você faz, e os seus planos serão bem-sucedidos'. (Pv 16.3)";
+palavras[9] = "'Consagre ao Senhor tudo o que você faz, e os seus planos serão bem-sucedidos'. <br>(Pv 16.3)</br>";
 
-palavras[10] = "'Quem tem muitos amigos pode chegar à ruína, mas existe amigo mais apegado que um irmão'. (Pv 18.24)";
+palavras[10] = "'Quem tem muitos amigos pode chegar à ruína, mas existe amigo mais apegado que um irmão'. <br>(Pv 18.24)</br>";
 
-palavras[11] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. (Rm 12.1)";
+palavras[11] = "'Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus'. <br>(Rm 12.1)</br>";
 
-palavras[12] = "'Então respondeu Jesus, e disse-lhe: Ó mulher, grande é a tua fé! Seja isso feito para contigo como tu desejas. E desde aquela hora a sua filha ficou sã'. (Mt 15.28)"; 
+palavras[12] = "'Então respondeu Jesus, e disse-lhe: Ó mulher, grande é a tua fé! Seja isso feito para contigo como tu desejas. E desde aquela hora a sua filha ficou sã'. <br>(Mt 15.28)</br>"; 
 
-palavras[13] = "'Deixando, pois, toda a malícia, e todo o engano, e fingimentos, e invejas, e todas as murmurações,Desejai afetuosamente, como meninos novamente nascidos, o leite racional, não falsificado, para que por ele vades crescendo'. (1 Pd 2.1,2)"; 
+palavras[13] = "'Deixando, pois, toda a malícia, e todo o engano, e fingimentos, e invejas, e todas as murmurações,Desejai afetuosamente, como meninos novamente nascidos, o leite racional, não falsificado, para que por ele vades crescendo'. <br>(1 Pd 2.1,2)</br>"; 
 
-palavras[14] = "'E, chegando-vos para ele, pedra viva, reprovada, na verdade, pelos homens, mas para com Deus eleita e preciosa,Vòs também, como pedras vivas, sois edificados casa espiritual e sacerdòcio santo, para oferecer sacrifícios espirituais agradáveis a Deus por Jesus Cristo'. (1 Pd 2.4,5)"; 
+palavras[14] = "'E, chegando-vos para ele, pedra viva, reprovada, na verdade, pelos homens, mas para com Deus eleita e preciosa,Vòs também, como pedras vivas, sois edificados casa espiritual e sacerdòcio santo, para oferecer sacrifícios espirituais agradáveis a Deus por Jesus Cristo'. <br>(1 Pd 2.4,5)</br>"; 
 
-palavras[15] = "'Procura apresentar-te a Deus aprovado, como obreiro que não tem de que se envergonhar, que maneja bem a palavra da verdade'. (2 Tm 2:15)"; 
+palavras[15] = "'Procura apresentar-te a Deus aprovado, como obreiro que não tem de que se envergonhar, que maneja bem a palavra da verdade'. <br>(2 Tm 2:15)</br>"; 
+
+palavras[16] = "'E o Senhor te guiará continuamente, e fartará a tua alma em lugares secos, e fortificará teus ossos; e serás como um jardim regado e como um manancial cujas águas nunca faltam. '. <br>(Is 58.11)</br>"; 
+
+palavras[17] = "'Eis que farei uma coisa nova, e, agora, sairá à luz; porventura, não a sabereis? Eis que porei um caminho no deserto e rios, no ermo. '. <br>(Is 43.19)</br>"; 
+
+palavras[18] = "' Eu irei adiante de ti, e endireitarei os caminhos tortos; quebrarei as portas de bronze e despedaçarei os ferrolhos de ferro. 3 - E te darei os tesouros das escuridades e as riquezas encobertas, para que possas saber que eu sou o Senhor, o Deus de Israel, que te chama pelo teu nome.'. <br>(Is 45:2-3)</br>"; 
+
+palavras[19] = "'Não se turbe o vosso coração; credes em Deus, crede também em mim.'. <br>(Jo 14.1)</br>"; 
+
+palavras[20] = "'Como escaparemos nós, se não atentarmos para uma tão grande salvação, a qual, começando a ser anunciada pelo Senhor, foi-nos, depois, confirmada pelos que a ouviram;'. <br>(Hb 2.3)</br>"; 
+
+palavras[21] = "' Toda ferramenta preparada contra ti não prosperará; e toda língua que se levantar contra ti em juízo, tu a condenarás; esta é a herança dos servos do Senhor e a sua justiça que vem de mim, diz o Senhor.'. <br>(Is 54.17)</br>"; 
+
+palavras[22] = "'Temos, porém, esse tesouro em vasos de barro, para que a excelência do poder seja de Deus e não de nós. 8 - Em tudo somos atribulados, mas não angustiados; perplexos, mas não desanimados; 9 - perseguidos, mas não desamparados; abatidos, mas não destruídos; 10 - trazendo sempre por toda parte a mortificação do Senhor Jesus no nosso corpo, para que a vida de Jesus se manifeste também em nossos corpos. '. <br>(2 co 4.7-10)</br>"; 
 
 
 
-botaoSortear.addEventListener("click", ()=>{
+btn.addEventListener("click", ()=>{
 
-numeroSorteado.innerHTML =  palavras[
+    palavraSortida.innerHTML =  palavras[
 Math.floor(Math. random() * palavras.length)
 
 ]
